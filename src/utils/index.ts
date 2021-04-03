ebb4e908fd975c9208906c81ab442288bb429142
HolyCat <wpf19970716@gmail.com>
2021-04-04 00:06:01 +0800

    调整一下项目结构相关

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,52 +1,16 @@
-import { InitializeException } from "../error/initialize-exception";
+import fetch from 'node-fetch';
 
-const SPRING = Symbol("SPRING#GLOBAL#CONFIG");
+type RequestOptions = {
+    method?: string;
+    body?: string;
+    headers?: Record<string, string>,
 
-const SPRING_HOOKS = {
-  INITIALIZE: "initialize",
-  START: "start",
-  DOWNLOADING: "downloading",
-  DOWNLOADED: "downloaded",
-  ITEM_CREATE: 'item:create',
-  ITEM_UPDATE: 'item:update',
-  RESTART: "restart",
-  STOP: "stop",
-};
-
-function def(obj: any, key: PropertyKey, config: PropertyDescriptor) {
-  return Object.defineProperty(obj, key, config);
 }
 
-export function getSpringGlobal(): SpringGlobal {
-  if (!globalThis[SPRING]) {
-    throw new InitializeException();
-  }
-  return globalThis[SPRING];
-}
+class HttpUtils {
+    
 
-class SpringGlobal {
-  
-  private subscriberRecord: Record<string, Array<Function>> = {};
-
-  constructor() {}
-
-  on(evtName: string, subscriber: Function) {
-    if (!this.subscriberRecord[evtName]) {
-      this.subscriberRecord[evtName] = [];
-    }
-    const subscriberArray = this.subscriberRecord[evtName];
-    if (!subscriberArray.includes(subscriber)) {
-      subscriberArray.push(subscriber);
-    }
-  }
-
-  static create() {
-    return new SpringGlobal()
-  }
-}
-
-export function initializeSpringGlobal() {
-    if (!globalThis[SPRING]) {
-        globalThis[SPRING] = SpringGlobal.create();
+    static fetch(url: string, opts: RequestOptions) {
+        return fetch(url, opts)
     }
 }
